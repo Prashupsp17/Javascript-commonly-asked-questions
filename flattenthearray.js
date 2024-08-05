7e5350d8256bbbc01d319c7c9348513363d4deb2
Prashupsp17 <shindeprashant967@gmail.com>
2024-08-05 06:56:02 +0530

    Flatten The Array

--- flattenthearray.js
+++ flattenthearray.js
@@ -1,42 +1,16 @@
-function flattenArray(arr) {
-    let result = [];
+const nestedArray = [1,[2,3,4,[5,6],[7,[8,[9]]]]];
 
-    function flatten(item) {
-        if (typeof item === 'object' && item !== null) {
-            for (let i = 0; i < item.length; i++) {
-                flatten(item[i]);
-            }
-        } else {
-            result.push(item);
+function flattenArray(input){
+    let flattenedArr = [];
+
+    for(let i=0;i<input.length;i++){
+        if(Array.isArray(input[i])){
+            flattenedArr = flattenedArr.concat(flattenArray(input[i]));
+        }else{ 
+            flattenedArr.push(input[i]);
         }
     }
+    return flattenedArr;
 
-    flatten(arr);
-    return result;
 }
-
-// Example usage:
-let nestedArray = [1, [2, [3, 4], 5], 6];
-let flattened = flattenArray(nestedArray);
-console.log(flattened); // Output: [1, 2, 3, 4, 5, 6]
-
-
-
-// const deeplyNestedArray = [
-//     1,
-//     [2, 3],
-//     [
-//         4,
-//         [5, 6, [7, 8]],
-//         [9, [10, [11, [12]]]]
-//     ],
-//     [
-//         [13],
-//         [[14, 15], 16],
-//         [17, [18, [19, [20]]]]
-//     ]
-// ];
-
-
-// const result = deeplyNestedArray.flat(5);
-// console.log(result);
+console.log(flattenArray(nestedArray));
