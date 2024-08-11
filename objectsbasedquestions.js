5d5b2ceb5239e24c970e7df452793a7eef407aaf
Prashupsp17 <shindeprashant967@gmail.com>
2024-08-11 17:33:38 +0530

    Objects based Questions

---
+++ objectsbasedquestions.js
@@ -0,0 +1,278 @@
+const data = {
+        "menuNameVsId": {
+            "Tuna Maki Roll": 13953,
+            "Yellowtail Maki Roll": 13954,
+            "Salmon Maki Roll": 13955,
+            "Tuna Hand Roll": 13956,
+            "Yellowtail Hand Roll": 13957,
+            "Salmon Hand Rolls": 13958,
+            "Spicy Scallop Hand Roll": 13959,
+            "Spicy Tuna Hand Roll": 13960,
+            "Spicy Hamachi Hand Roll": 13961,
+            "Salmon Skin Hand Roll": 13962,
+            "Shrimp Tempura Hand Rolls": 13963,
+            "California Hand Roll": 13964,
+            "Veggie Hand Roll": 13965,
+            "CALIFORNIA CLASSIC ROLLS": 13966,
+            "TUNA CLASSIC ROLLS": 13967,
+            "SALMON CLASSIC ROLLS": 13968,
+            "Filter By Menu": 0,
+            "Miso soup": 13925,
+            "BROWN RICE": 13926,
+            "meal for 3 test": 13928,
+            "Meal for veg entrees": 13929,
+            "AGEDASHI TOFU": 13931,
+            "POTSTICKERS (6)": 13932,
+            "SHRIMP SHUMAI (6)": 13933,
+            "MONKEY BALLS": 13934,
+            "TEMPURA": 13935,
+            "CENTIPEDE VEGETARIAN ROLL": 14000,
+            "PICANTE VEGETARIAN ROLL": 14001,
+            "SESAME TATAKI  SASHIMI": 14002,
+            "TIJUANA TATAKI  SASHIMI": 14003,
+            "MACHO TAKO  SASHIMI": 14004,
+            "RAJUN CAJUN  SASHIMI": 14005,
+            "SUNSHINE  SASHIMI": 14006,
+            "GOLDEN MONKEY  SASHIMI": 14007,
+            "CHEF SASHIMI": 14008,
+            "Tuna ": 14009,
+            "Yellowtail": 14010,
+            "Albacore": 14011,
+            "Salmon": 14012,
+            "Smoked Salmon": 14013,
+            "Mackerel ": 14014,
+            "Shrimp": 14015,
+            "Snow Crab": 14016,
+            "Salmon Roe": 14017,
+            "Octopus": 14018,
+            "Squid": 14019,
+            "Smelt Roe": 14020,
+            "Halibut": 14021,
+            "KAMA": 13936,
+            "CALAMARI": 13937,
+            "SPICY CEVICHE": 13938,
+            "POKE TARTAR": 13939,
+            "SEAWEED": 13941,
+            "THE COVE": 13943,
+            "SASHIMI SALAD": 13945,
+            "BENTO BOX ": 13946,
+            "HOUSE FRIED RICE": 13947,
+            "YAKISOBA": 13948,
+            "TERIYAKI BOWL": 13950,
+            "Avocado Maki Roll": 13951,
+            "Cucumber Maki Roll": 13952,
+            "YELLOWTAIL CLASSIC ROLLS": 13969,
+            "PHILADELPHIA CLASSIC ROLLS": 13970,
+            "SPICY TUNA CLASSIC ROLLS": 13971,
+            "SPICY HAMACHI CLASSIC ROLLS": 13972,
+            "CRUNCHY CLASSIC ROLLS": 13973,
+            "SHRIMP TEMPURA CLASSIC ROLLS": 13974,
+            "RAINBOW CLASSIC ROLLS": 13975,
+            "SPYDER CLASSIC ROLLS": 13976,
+            "CATERPILLAR CLASSIC ROLLS": 13977,
+            "COCO SPECIALTY ROLL": 13978,
+            "BARRIO SPECIALTY ROLL": 13979,
+            "OZSTARS SPECIALTY ROLL": 13980,
+            "KITCHEN SINK SPECIALTY ROLL": 13981,
+            "SLIPPERY WHEN WET SPECIALTY ROLL": 13982,
+            "SWEET LEW SPECIALTY ROLL": 13983,
+            "CHRONIC SPECIALTY ROLL": 13984,
+            "ORANGE CRUSH SPECIALTY ROLL": 13985,
+            "BRUCE LEE SPECIALTY ROLL": 13986,
+            "BIG NICK SPECIALTY ROLL": 13987,
+            "SUNKIST SPECIALTY ROLL": 13988,
+            "THE VIKING SPECIALTY ROLL": 13989,
+            "THE NIGHTHAWK SPECIALTY ROLL": 13990,
+            "ROCK N ROLL SPECIALTY ROLL": 13991,
+            "RED DRAGON SPECIALTY ROLL": 13992,
+            "DON SPECIALTY ROLL": 13993,
+            "ASHLEY SPECIALTY ROLL": 13994,
+            "BLONDIE SPECIALTY ROLL": 13995,
+            "THE ROCK protein roll SPECIALTY ROLL": 13996,
+            "THE FIRECRACKER SPECIALTY ROLL": 13997,
+            "GONZO SPECIALTY ROLL": 13998,
+            "GREEN ROOM VEGETARIAN ROLL": 13999,
+             "Spicy Scallop": 14022,
+            "Fresh Water Eel": 14023,
+            "Tamago": 14024,
+            "SWEET POTATO FRIES (HAPPY hour)": 14025,
+            "POTSTICKERS (HAPPY hour)": 14026,
+            "CRISPY VEGGIE SPRING ROLLS (HAPPY hour)": 14027,
+            "ASIAN WINGS (HAPPY hour)": 14028,
+            "CALAMARI (HAPPY hour)": 14029,
+            "RICE (HAPPY hour)": 14030,
+            "MISO SOUP (HAPPY hour)": 14031,
+            "SPICY TUNA SUSHI (HAPPY hour)": 14033,
+            "CRUNCHY SUSHI (HAPPY hour)": 14034,
+            "COCO SUSHI (HAPPY hour)": 14035,
+            "VEGETARIAN HAND ROLL  (HAPPY hour)": 14036,
+            "SPICY TUNA HAND ROLL  (HAPPY hour)": 14037,
+            "SALMON SKIN HAND ROLL  (HAPPY hour)": 14038,
+            "SHRIMP TEMPURA HAND ROLL  (HAPPY hour)": 14039,
+            "SPICY KRAB HAND ROLL  (HAPPY hour)": 14040,
+            "Yakisoba (Lunch) ": 14041,
+            "Bento Box (Lunch)": 14042,
+            "Fish Tacos(2) (Lunch)": 14043,
+            "Teriyaki Bowl (Lunch)": 14044,
+            "Any 3 Entree": 14212,
+            "test": 14214,
+            "Lunch Combo": 15192,
+            "JKLMNOP": 15458,
+            "Schezwan": 15463,
+            "Fried": 15464,
+            "Pazta": 15465,
+            "Matter Paneer Side": 15664,
+            "Saag Paneer Side": 15665,
+            "Victor Chicken": 20139,
+            "hummus and pita": 20140,
+            "Pita Bread": 20141,
+            "Victors Chicken Salad": 20142,
+            "Feta": 20143,
+            "The Kabob Plate": 20144,
+            "House Special Soup": 20608,
+            "Zero Price Menu": 20807,
+            "Green Tea": 21100,
+            "Basundi": 22015,
+            "Kids Buffet": 22299,
+            "Test Menu": 22302,
+            "Menu": 22435,
+            "Paneer Tikka Masala": 22706,
+            "Tom Kah": 22709,
+            "Special Meal for 6-Spice Level Test": 22740,
+            "select any 4 entrees-Veg,paneer,chicken and lamb(10+persons)": 22766,
+            "test for dish validation": 23505,
+            "valentine\u0027s day special meal for 2": 23542,
+            "valentine\u0027s day special meal for 4": 23543,
+            "menu2 for multiselect": 26082,
+            "multiselect for meal 3": 26083,
+            "Special Meal For 2": 26094,
+            "Menu test": 26188,
+            "test ": 26189,
+            "select menu ": 27180,
+            "test menu for test": 27232,
+            "menu test": 27239,
+            "TestMenu1": 27240,
+            "Bugur1": 27242,
+            "veg sandwich": 27243,
+            "menu by netra": 28143,
+            "new menu": 28144,
+            "MENU FOR TEST ": 28232,
+            "TEST TODAY MENU": 28551,
+            "samosa 1": 28552,
+            "Jalebi": 28553,
+            "New paneer samosa": 30611,
+            "Spicy Sesame Ramen": 31550,
+            "Blueberry": 33717,
+            "Amazing Chow Noodle": 33836,
+            "test menu": 34868,
+            "Burgur": 34869,
+            "meal for 3": 34870,
+            "loving strips": 35478,
+            "Loving Strips w/ Fries": 35479,
+            "New menu for reusable": 36023,
+            "new menu from pos": 36024,
+            "SHAKE": 36351,
+            "Add strips": 36376,
+            "Blissful Pho": 36381,
+            "Amazing Chow": 36383,
+            "Kombucha on Tap": 36385,
+            "Avocado BLT Sandwich": 36388,
+            "Danish": 36390,
+            "Outstanding": 36393,
+            "Wild West": 36395,
+            "Unreal Chocolate": 36397,
+            "Pocket Latte": 36399,
+            "pocket latte coffee Nuts": 36400,
+            "Wai Lana Chips": 36402,
+            "Combo 1": 37206,
+            "Tomato chutni test": 37222,
+            "Special Paneer test": 37224,
+            "Mango juice": 37227,
+            "Corn Sandwich": 37229,
+            "Green salad": 37230,
+            "red salad": 37231,
+            "new saladsss": 37232,
+            "red wine": 37233,
+            "NEW SETUP MENU": 37290,
+            "NEW SETUP 2": 37291,
+            "NEW SETUP MENU3": 37292,
+            "today menu": 37336,
+            "POS NEW MENU2": 37337,
+            "new pos menu334": 37338,
+            "POS biryani": 37339,
+            "Aloo Mutter Paneer": 37343,
+            "Aloo Palak Paneer": 37344,
+            "Aloo Masala": 37345,
+            "POS WINE TEST": 37469,
+            "Test Red Wine": 37470,
+            "Dry Bhel": 37472,
+            "Samosa": 37477,
+            "Chocolate": 37478,
+            "Butterscotch": 37479,
+            "POS test Wine for test": 37500,
+            "NEW N DUM BIRYANI \u0027": 37541,
+            "Add new menu from NA": 37543,
+            "Vada Pav with chutney": 37544,
+            "Samosa with  chutney": 37909,
+            "Bhel": 37912,
+            "Combo A": 37943,
+            "Masala chai": 38211,
+            "Coffee": 38212,
+            "chicken roll  thai": 38411,
+            "chickennew roll thai": 38412,
+            "T1": 38413
+        }
+}
+
+// console.log(data.menuNameVsId);
+
+// iterate over key value pairs
+const arr = [];
+for(let [key,value] of Object.entries(data.menuNameVsId)){
+   arr.push(`Menu Name:${key} :${value} `);
+} 
+// console.log(arr);
+
+// ADD
+data.menuNameVsId["Prashant"] = 123;
+
+// DELETE 
+delete data.menuNameVsId["Prashant"];
+// console.log(data.menuNameVsId);
+
+const valuesArr = [];
+for(let value in Object.values(data.menuNameVsId)){
+   valuesArr.push(value);
+}
+// console.log(valuesArr);
+
+const values = Object.values(data.menuNameVsId);
+// console.log(newArr);
+
+const keys = Object.keys(data.menuNameVsId);
+// console.log(keys);
+
+let entries = Object.entries(data.menuNameVsId);
+// console.log(entries);
+
+for(let i=0;i<entries.length;i++){
+    for(let j=i+1;j<entries.length;j++){
+        if(entries[i][1] > entries[j][1]){
+            let temp  =entries[i];
+            entries[i] = entries[j];
+            entries[j] = temp;
+        }
+    }
+}
+
+const sortDataBYValues = Object.fromEntries(entries);
+// console.log(sortDataBYValues);
+
+for(let value in data.menuNameVsId ){
+    if(data.menuNameVsId[value] > 0){
+        delete data.menuNameVsId[value];
+    }
+}
+console.log(data.menuNameVsId);
+
+
