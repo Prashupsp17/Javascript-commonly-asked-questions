dee451c6bd4524d2268ee50eae303be6fa86d6fa
Prashupsp17 <shindeprashant967@gmail.com>
2024-09-09 07:26:50 +0530

    New Way

--- reversewordsinastring.js
+++ reversewordsinastring.js
@@ -3,6 +3,24 @@
 const input = "Lets take leetcode contest";
 const output = "stes ekat edocteel testnoc";
 
+function reverse(input){
+    const x = input.split(" ");
+    const str= [];
+
+    for(let i=0;i<x.length;i++){
+        let char = x[i];
+
+        let reversedWord = "";
+
+        for(let i=char.length-1;i>=0;i--){
+            reversedWord += char[i];
+        }
+        str.push(reversedWord);
+    }
+    return str.join(" ");
+}
+console.log(reverse(input));
+
 var reverseWords = function(s) {
     const str = [];
     const words = s.split(" ");
