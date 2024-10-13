c1718f72869eb69ea9ec2e6f2eea88b7a05c14a6
Prashupsp17 <shindeprashant967@gmail.com>
2024-10-13 16:23:42 +0530

    Rotate Array By K

--- perscitussolutions.js
+++ perscitussolutions.js
@@ -31,3 +31,18 @@ function getOutput(str){
 
 }
 console.log(getOutput(str));
+
+const arr = [7,5,3,2,1,6];
+const n = 2;
+
+function rotateArrByK(arr,n){
+
+    let result = [];
+
+    for(let i=0;i<n;i++){
+        result.unshift(arr.pop());
+    }
+    return result.concat(arr);
+
+}
+console.log(rotateArrByK(arr,n));
