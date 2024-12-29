06f92ead664d6b33da74decaa64abcab5838b8d3
Prashupsp17 <shindeprashant967@gmail.com>
2024-12-29 12:03:22 +0530

    New Solution as asked

--- perscitussolutions.js
+++ perscitussolutions.js
@@ -1,36 +1,96 @@
-const str = "My name is prashant shinde 1239";
+//originial Questions
+// let str = "my name is yash and my customerid is 1239";
+// output = "ym eman si hsay dna ym diremotsuc si one two three nine"
 
-function getOutput(str){
+const str = "my name is yash and my customerid is 1239";
 
-    let obj = {
+function output (str) {
+    
+    
+    const obj = {
         1:"one",
         2:"two",
         3:"three",
         9:"nine"
     }
-
-    let arr = str.split(" ");
-    let result = [];
-
+    
+    const arr = str.split(" ");
+    const result = [];
+    
     for(let i=0;i<arr.length;i++){
-        let word= arr[i];
-        let reversed = " ";
-
-        for(let j=word.length-1;j>=0;j--){
+        let word = arr[i];
+        
+        let reversed="";
+        let numWord = "";
+        
+        if(/\d/.test(word)){
+            
+            for(let j=0;j<word.length;j++){
+                let char  = word[j];
+                
+                if(obj[char]){
+                    numWord += obj[char] + " ";
+                }
+            }
+            result.push(numWord.trim());
+        
+            
+        }else{
+            for(let j=word.length-1;j>=0;j--){
             let char = word[j];
-            if(obj[char]){
-                reversed += `${obj[char]} `;
-            }else{
                 reversed += char;
             }
-           
+             result.push(reversed);
+            
         }
-        result.push(reversed);
+        
+        
+       
+       
     }
     return result.join(" ");
-
+   
+    
 }
-console.log(getOutput(str));
+
+console.log(output(str));
+
+
+// const str = "My name is prashant shinde 1239";
+
+// function getOutput(str){
+
+//     let obj = {
+//         1:"one",
+//         2:"two",
+//         3:"three",
+//         9:"nine"
+//     }
+
+//     let arr = str.split(" ");
+//     let result = [];
+
+//     for(let i=0;i<arr.length;i++){
+//         let word= arr[i];
+//         let reversed = " ";
+
+//         for(let j=word.length-1;j>=0;j--){
+//             let char = word[j];
+//             if(obj[char]){
+//                 reversed += `${obj[char]} `;
+//             }else{
+//                 reversed += char;
+//             }
+           
+//         }
+//         result.push(reversed);
+//     }
+//     return result.join(" ");
+
+// }
+// console.log(getOutput(str));
+
+
 
 const arr = [7,5,3,2,1,6];
 const n = 2;
