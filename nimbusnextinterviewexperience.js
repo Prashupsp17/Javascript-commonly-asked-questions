d4a4be95b2f8b6a30e82fa5d74188a29687d579b
Prashupsp17 <shindeprashant967@gmail.com>
2025-01-11 17:01:01 +0530

    with and without inbuilt methods

--- nimbusnextinterviewexperience.js
+++ nimbusnextinterviewexperience.js
@@ -9,7 +9,7 @@
 // 7 :  Tell about modules you worked on ?
 // 8: How u will get to know about resolution ?
 
-// 9 : Write a program to find unique and reverse the array?
+// 9 : Write a program to find unique and reverse the array using inbuilt and without inbuilt?
 
 //  const arr = [1,1,2,2,3,3,4,4,5,5];
 
@@ -19,3 +19,38 @@
 // console.log(uniqueArr);
 // const reversedArr = uniqueArr.reverse();
 // console.log(reversedArr);
+
+
+// const arr = [1,1,2,2,3,4,5,6];
+
+// function output(arr){
+    
+//     let obj = {};
+//     let result = [];
+    
+//     for(let i=0;i<arr.length;i++){
+//         let num = arr[i];
+        
+//         if(obj[num] > 0){
+//             obj[num]++;
+//         }else{
+//             obj[num] = 1;
+//         }
+//     }
+    
+    
+//     for(let [key,value] of Object.entries(obj)){
+//         if(value === 1){
+//             result.push(+key);
+//         }
+//     }
+    
+//     let reversedArr = [];
+    
+//     for(let i=result.length-1;i>=0;i--){
+//         reversedArr.push(result[i]);
+//     }
+//     return reversedArr;
+    
+// }
+// console.log(output(arr));
