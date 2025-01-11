044b27ceb58acc845649ca69139c046fc1b9cb7b
Prashupsp17 <shindeprashant967@gmail.com>
2025-01-11 16:50:29 +0530

    NimbuxNext Interview Experience

---
+++ nimbusnextinterviewexperience.js
@@ -0,0 +1,21 @@
+// 1 : 8 Array Methods
+// 1: ForEach, Map, Slice, Splice,push,pop, shift,unshift,filter,reduce
+
+// 2: difference between shift and pop
+// 3: slice and splice
+// 4: shallow Copy and Deep copy
+// 5: How to manipulate DOM ?
+// 6: Redux 
+// 7 :  Tell about modules you worked on ?
+// 8: How u will get to know about resolution ?
+
+// 9 : Write a program to find unique and reverse the array?
+
+//  const arr = [1,1,2,2,3,3,4,4,5,5];
+
+// const newArr = new Set(arr);
+// console.log(newArr);
+// const uniqueArr = [...newArr];
+// console.log(uniqueArr);
+// const reversedArr = uniqueArr.reverse();
+// console.log(reversedArr);
