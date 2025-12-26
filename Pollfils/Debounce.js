6c94f57850cc8e31bd437d6ce154030c41604c0a
Prashupsp17 <shindeprashant967@gmail.com>
2025-12-26 17:34:44 +0530

    Throttle also implemented

--- Pollfils/Debounce.js
+++ Pollfils/Debounce.js
@@ -2,6 +2,7 @@
 const increment_btn = document.querySelector(".increment_btn");
 const increment_pressed = document.querySelector(".increment_pressed");
 const increment_count = document.querySelector(".increment_count");
+const throttle_increment_btn = document.querySelector(".throttle_increment_btn");
 
 let pressedCount = 0;
 let triggeredCount = 0;
@@ -31,3 +32,26 @@ increment_btn.addEventListener("click", () => {
     increment_pressed.innerHTML = ++pressedCount;
      debounce();
 })
+
+
+const myThrottle = (cb,d) => {
+    let last = 0;
+
+    return function(...args){
+        let  now = new Date().getTime();
+        if(now - last < d) return;
+        last = now;
+        return cb(...args);
+    }
+}
+
+var Throttle = myThrottle(() => {
+    triggeredCount += 1;
+    increment_count.innerHTML = triggeredCount;
+},1000);
+
+
+throttle_increment_btn.addEventListener("click", () => {
+    increment_pressed.innerHTML = ++pressedCount;
+    Throttle();
+});
