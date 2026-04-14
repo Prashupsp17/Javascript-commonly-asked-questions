6d6e803b72f2e90746adb692e18cfda001e54a1a
Prashupsp17 <shindeprashant967@gmail.com>
2026-04-14 11:25:09 +0530

    144. Binary Tree Preorder Traversal iterative approach

--- Binary Tree/144BinaryTreePreorderTraversal.js
+++ Binary Tree/144BinaryTreePreorderTraversal.js
@@ -74,3 +74,21 @@ var preorderTraversal = function(root) {
     traversal(root);
 }
 console.log(preorderTraversal([1,null,2,3]));
+
+
+// Iterative approach 
+
+// var preorderTraversal = function(root) {
+//     if(!root) return [];
+//     let ans = [];
+//     let stack = [root];
+
+//     while(stack.length){
+//         let curr = stack.pop();
+//         ans.push(curr.val);
+//         curr.right && stack.push(curr.right);
+//         curr.left && stack.push(curr.left);
+//     }
+//     return ans;
+    
+// };
