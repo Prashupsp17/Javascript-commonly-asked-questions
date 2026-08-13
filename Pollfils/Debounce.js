933bebdc9b8581140c61a3a0f75c741f664dd17d
Prashant Shinde <121381323+Prashupsp17@users.noreply.github.com>
2026-08-13 13:03:19 +0530

    Refactor debounce code with input
    Refactored debounce and throttle functions, removed unused elements and event listeners.

--- Pollfils/Debounce.js
+++ Pollfils/Debounce.js
@@ -1,57 +1,25 @@
+const input = document.querySelector("#search");
 
-const increment_btn = document.querySelector(".increment_btn");
-const increment_pressed = document.querySelector(".increment_pressed");
-const increment_count = document.querySelector(".increment_count");
-const throttle_increment_btn = document.querySelector(".throttle_increment_btn");
+function debounce(fn,delay){
 
-let pressedCount = 0;
-let triggeredCount = 0;
+  let timer;
 
+  return function(...args){
+    clearTimeout(timer);
 
+    timer = setTimeout(() => {
+      fn(...args);
+    },delay);
+  }
 
-const myDebounce = (cb,d) => {
-
-    let timer;
-
-    return function(...args){
-        if(timer) clearTimeout(timer);
-        timer = setTimeout(() => {
-            cb(...args);
-        },d)
-    };
-
-};
-
-const debounce = myDebounce(() => {
-    triggeredCount += 1;
-    increment_count.innerHTML = triggeredCount;
-},800);
-
-
-increment_btn.addEventListener("click", () => {
-    increment_pressed.innerHTML = ++pressedCount;
-     debounce();
-})
-
-
-const myThrottle = (cb,d) => {
-    let last = 0;
-
-    return function(...args){
-        let  now = new Date().getTime();
-        if(now - last < d) return;
-        last = now;
-        return cb(...args);
-    }
 }
 
-var Throttle = myThrottle(() => {
-    triggeredCount += 1;
-    increment_count.innerHTML = triggeredCount;
-},1000);
+function search(value){
+   console.log(value);
+   alert(value);
+}
 
-
-throttle_increment_btn.addEventListener("click", () => {
-    increment_pressed.innerHTML = ++pressedCount;
-    Throttle();
-});
+const debounceSearch = debounce(search,5000);
+input.addEventListener("input",function(e){
+   debounceSearch(e.target.value);
+})
