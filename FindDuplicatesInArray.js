589d8869b15ce5b4b3d0270ef69d77fe6cb3629a
Prashant Shinde <121381323+Prashupsp17@users.noreply.github.com>
2026-08-14 11:22:09 +0530

    Implement findDuplicates function
    Added a function to find duplicates in an array.

--- FindDuplicatesInArray.js
+++ FindDuplicatesInArray.js
@@ -1,3 +1,23 @@
+// 1st way 
+function findDuplicates(arr){
+   let map = {};
+   let duplicates = [];
+
+   for(let i=0;i<arr.length;i++){
+      let item = arr[i];
+
+      if(map[[item]]){
+           duplicates.push(item);
+      }else{
+        map[item] = true;
+      }
+   }
+
+  return duplicates;
+}
+
+// 2nd Way
+
 const data = [10,20,30,40,10,20];
 
 function duplicates(data){
